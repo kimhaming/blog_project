c0f20349cdb0ff351e9b21806d1da5bbe67fd633
kimhaming <jimin24@student.hywoman.ac.kr>
2024-05-04 20:06:18 +0900

    feat: 성공 및 실패 테스트: mocking -> 빌더 패턴을 사용하여 오리지날 객체와 수정 요청 객체 생성 후 assertThat() 으로 검증 및 assertThrows() 으로 예상 에러 검증

--- src/test/java/me/kimhaming/springbootdeveloper/unit/service/BlogServiceUnitTest.java
+++ src/test/java/me/kimhaming/springbootdeveloper/unit/service/BlogServiceUnitTest.java
@@ -1,36 +1,31 @@
 package me.kimhaming.springbootdeveloper.unit.service;
-
 import me.kimhaming.springbootdeveloper.domain.Article;
-import me.kimhaming.springbootdeveloper.domain.BaseEntity;
-import me.kimhaming.springbootdeveloper.dto.AddArticleRequest;
-import me.kimhaming.springbootdeveloper.dto.ArticleResponse;
 import me.kimhaming.springbootdeveloper.dto.UpdateArticleRequest;
 import me.kimhaming.springbootdeveloper.repository.BlogRepository;
 import me.kimhaming.springbootdeveloper.service.BlogService;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.lang.reflect.Field;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-//@SpringBootTest
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class BlogServiceUnitTest {
 
-    @MockBean
+    @Mock
     private BlogRepository blogRepository;
 
     @InjectMocks
@@ -43,56 +38,48 @@ class BlogServiceUnitTest {
         @Test
         public void testUpdateSuccess() throws Exception {
             // given
-            long id = 1L;
+            // 빌더 패턴을 사용하여 테스트할 내용만 생성
+            // @Builder -> @SuperBuilder 변경해서 builder().createdAt() 불러오기 가능
+            // 목 객체를 생성하는 것은 아님
+            // findById() 메서드를 호출할 때 반환될 객체 만드는 것
+            Optional<Article> originalArticle = Optional.of(Article.builder()
+                    .createdAt(LocalDate.now().atStartOfDay().minusDays(8))
+                    .build());
 
-            // 수정 테스트에 사용할 가짜 글 생성
-            Article originalArticle = new Article("originalTitle",
-                    "originalContent",
-                    "google@gmail.com",
-                    "010-1234-5678",
-                    "홍길동",
-                    "asdf!@12345");
+            UpdateArticleRequest updateArticleRequest = UpdateArticleRequest.builder()
+                    .content("updatedContent")
+                    .build();
 
-            // 가짜 글 객체의 생성일자를 8일 전으로 설정
-            LocalDate createdDate = LocalDate.now().minusDays(8);
+            // when: 1번을 찾으면 무조건 오리지널 게시글이 반환되도록
+            when(blogRepository.findById(1L)).thenReturn(originalArticle);
 
-            // Reflection 사용하여 createdAt 필드에 접근하여 8일 전 생성일자 넣기
-            Field createdAtField = Article.class.getDeclaredField("createdAt");
-            createdAtField.setAccessible(true);
-            createdAtField.set(originalArticle, createdDate);
+            // 1번 글을 수정하고싶은 내용으로 update() 시켜서 Article 타입의 객체에 담기
+            Article updatedArticle = blogService.update(1L, updateArticleRequest);
 
-            // 수정할 내용을 담은 객체 생성
-            UpdateArticleRequest updateRequest = new UpdateArticleRequest(
-                    "updateTitle",
-                    "updateContent"
-            );
-
-            // 기대하는 값
-            Article expectedArticle = new Article("updateTitle",
-                    "updatedContent",
-                    "google@gmail.com",
-                    "010-1234-5678",
-                    "홍길동",
-                    "asdf!@12345");
-
-            // 글 수정 요청에 대한 가짜 응답
-            // 특정 고유 식별자가 입력된 경우 테스트 케이스에서 생성한 객체를 리턴한다
-            // id를 찾으면 무조건 originalArticle을 반환한다
-            given(blogRepository.findById(id)).willReturn(Optional.of(originalArticle));
-            given(blogService.update(id, updateRequest)).willReturn(expectedArticle);
-
-            // when: 글 수정 요청 보내기
-            Article updatedArticle = blogService.update(id, updateRequest);
-
-            // then: 저장된 아이디에 수정된 객체로 업데이트됐는지 확인하기
-            assertThat(updatedArticle.getTitle()).isEqualTo(expectedArticle.getTitle());
-            assertThat(updatedArticle.getContent()).isEqualTo(expectedArticle.getContent());
+            // then: 해당 메서드를 통해 업데이트된 값과 업데이트하고자 했던 값이 같은지 검증하기
+            assertThat(updatedArticle.getContent()).isEqualTo("updatedContent");
+            verify(blogRepository).findById(1L);
         }
 
         @DisplayName("생성일자로부터 10일 이상 됐을 때")
         @Test
         public void testUpdateFail() {
+            // given: 오리지널 객체와 수정 요청 객체 만들기
+            Optional<Article> originalArticle = Optional.of(Article.builder()
+                    .createdAt(LocalDate.now().atStartOfDay().minusDays(10))
+                    .build());
 
+            UpdateArticleRequest updateArticleRequest = UpdateArticleRequest.builder()
+                    .content("updatedContent")
+                    .build();
+
+            // when: 레파지토리에서 1번 찾으면 만들어둔 오리지널 객체 반환하도록 정하기
+            when(blogRepository.findById(1L)).thenReturn(originalArticle);
+
+            // assertThat(updatedArticle.getContent()).isEqualTo("updatedContent");
+            // 같은지 검증하면 실패가 뜨기 때문에, 발생할 예외가 나타나면 성공하도록 해야한다
+            // 1번 객체를 update() 시키려고 할 때 예측 가능한 예외 IllegalArgumentException 발생하는지 검증하기
+            assertThrows(IllegalArgumentException.class, () -> blogService.update(1L, updateArticleRequest));
 
         }
     }
