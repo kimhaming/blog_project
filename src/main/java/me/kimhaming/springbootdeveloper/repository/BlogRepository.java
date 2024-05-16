793bf693615a95f7e619de628ccb24fa576ad426
kimhaming <jimin24@student.hywoman.ac.kr>
2024-05-17 01:29:59 +0900

    feat: createdAt 기준 페이지네이션된 객체 찾아오는 메서드 생성

--- src/main/java/me/kimhaming/springbootdeveloper/repository/BlogRepository.java
+++ src/main/java/me/kimhaming/springbootdeveloper/repository/BlogRepository.java
@@ -1,9 +1,16 @@
 package me.kimhaming.springbootdeveloper.repository;
 
 import me.kimhaming.springbootdeveloper.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
+    Article findByTitle(String title);
+    List<Article> findByTitleContaining(String title);
+    Page<Article> findAll(Pageable pageable);
 }
