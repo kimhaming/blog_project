f60971a666d5124ed7bc1123687990e09e627f0b
kimhaming <jimin24@student.hywoman.ac.kr>
2024-05-24 18:31:24 +0900

    (WIP) 오타로 인한 오류 해결

--- src/main/java/me/kimhaming/springbootdeveloper/controller/TokenApiController.java
+++ src/main/java/me/kimhaming/springbootdeveloper/controller/TokenApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-RequiredArgsConstructor
+@RequiredArgsConstructor
 @RestController
 public class TokenApiController {
     private final TokenService tokenService;
