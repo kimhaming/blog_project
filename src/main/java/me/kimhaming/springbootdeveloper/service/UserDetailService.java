f38882f84950373fc408aac8e916c052ca1e5d66
kimhaming <jimin24@student.hywoman.ac.kr>
2024-07-02 18:19:15 +0900

    [refactor] 리턴 타입을 User 에서 UserDetails 로 변경: Spring Security와의 호환성 높임

--- src/main/java/me/kimhaming/springbootdeveloper/service/UserDetailService.java
+++ src/main/java/me/kimhaming/springbootdeveloper/service/UserDetailService.java
@@ -3,9 +3,12 @@ package me.kimhaming.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.kimhaming.springbootdeveloper.domain.User;
 import me.kimhaming.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -14,8 +17,14 @@ public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public User loadUserByUsername(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException(email));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("찾을 수 없는 이메일: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
     }
 }
