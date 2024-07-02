c4de3f55b2066fa473c3dfcf4b6b94736715f3c5
kimhaming <jimin24@student.hywoman.ac.kr>
2024-07-02 18:20:38 +0900

    [refactor] 회원가입 API에서 리다이렉트 방식 대신 ResponseEntity를 반환하여 HTTP 상태 코드와 메시지를 명시적으로 전달하도록 변경

--- src/main/java/me/kimhaming/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/kimhaming/springbootdeveloper/controller/UserApiController.java
@@ -2,26 +2,34 @@ package me.kimhaming.springbootdeveloper.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.kimhaming.springbootdeveloper.dto.AddUserRequest;
 import me.kimhaming.springbootdeveloper.service.UserService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api")
 public class UserApiController {
 
     private final UserService userService;
 
     // 회원가입 api
     @PostMapping("/user")
-    public String signup(AddUserRequest request) {
-        userService.save(request);
-        return "redirect:/login";   // redifrect: 라는 접두사 붙이면 강제로 /login URL에 해당하는 화면으로 이동
+    public ResponseEntity<?> signup(@Valid @RequestBody AddUserRequest request) {
+            userService.save(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
     }
 
     // 로그아웃 api
