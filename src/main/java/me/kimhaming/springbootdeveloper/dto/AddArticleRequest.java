eed0e35bf95cf1c456f9bdb0d4e6124cd004cae7
kimhaming <jimin24@student.hywoman.ac.kr>
2024-07-02 18:15:32 +0900

    [feat] 회원가입 시 요청받을 내용 & 게시글 등록 시 요청받을 내용 수정

--- src/main/java/me/kimhaming/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/kimhaming/springbootdeveloper/dto/AddArticleRequest.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.kimhaming.springbootdeveloper.domain.Article;
+import me.kimhaming.springbootdeveloper.domain.User;
 
 @NoArgsConstructor  //기본생성자
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자
@@ -21,30 +22,11 @@ public class AddArticleRequest {
     @NotBlank(message = "내용은 필수 입력 값입니다.")
     private String content;
 
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Email(message = "이메일 형식이 올바르지 않습니다.")
-    private String email;
-
-    private String phoneNum;
-
-    @NotBlank(message = "작성자 이름은 필수 입력 값입니다.")
-    private String author;
-
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9].*[0-9].*[0-9].*[0-9].*[0-9])(?=.*[*!@#$%].*[*!@#$%]).*$",
-            message = "비밀번호는 대소문자, 숫자 5개 이상, 특수문자 *!@#$% 중 2개 이상을 포함해야 합니다."
-    )
-    private String password;
-
     // dto -> entity로 변환
     public Article toEntity() {
         return Article.builder()
                 .title(title)
                 .content(content)
-                .email(email)
-                .phoneNum(phoneNum)
-                .author(author)
-                .password(password)
                 .build();
     }
 }
--- src/main/java/me/kimhaming/springbootdeveloper/dto/AddUserRequest.java
+++ src/main/java/me/kimhaming/springbootdeveloper/dto/AddUserRequest.java
@@ -1,5 +1,8 @@
 package me.kimhaming.springbootdeveloper.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +10,14 @@ import lombok.Setter;
 @Setter
 // 회원가입 request dto
 public class AddUserRequest {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     private String email;
+
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9].*[0-9].*[0-9].*[0-9].*[0-9])(?=.*[*!@#$%].*[*!@#$%]).*$",
+            message = "비밀번호는 대소문자, 숫자 5개 이상, 특수문자 *!@#$% 중 2개 이상을 포함해야 합니다."
+    )
     private String password;
 }
