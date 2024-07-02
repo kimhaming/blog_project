4fa18fc38773967f53edabb61305358faa445285
kimhaming <jimin24@student.hywoman.ac.kr>
2024-07-02 18:23:07 +0900

    [feat] User 엔티티에 phoneNum 및 roles (Spring Security 기반의 유연한 권한 관리를 지원하기 위해) 필드 추가

--- src/main/java/me/kimhaming/springbootdeveloper/domain/User.java
+++ src/main/java/me/kimhaming/springbootdeveloper/domain/User.java
@@ -1,7 +1,9 @@
 package me.kimhaming.springbootdeveloper.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,8 +16,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -34,15 +38,31 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column
+    private String phoneNum;
+
+    /**
+     * 값 타입 컬렉션으로 저장하기 위한 어노테이션
+     * RDBMS에는 컬렉션과 같은 형태의 데이터를 컬럼에 저장할 수 없기 때문에
+     * 별도의 테이블을 생성하여 컬렉션을 관리해야 한다.
+     * 이 때, 해당 필드가 컬렉션 객체임을 JPA에게 알려주는 어노테이션
+      */
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "USER_ROLES")
+    private List<String> roles = new ArrayList<>();
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, List<String> roles) {
         this.email = email;
         this.password = password;
+        this.roles = roles;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("user"));
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     // 사용자의 id를 반환(고유한 값)
